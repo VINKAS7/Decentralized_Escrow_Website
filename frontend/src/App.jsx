fb10a7dbeb25f8d0f0699f5fb7f98ff680e3c01b
Vinkas7 <109889653+VINKAS7@users.noreply.github.com>
2024-10-24 15:00:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import CreateEscrow from './components/CreateEscrow';
 import ContractList from './components/ContractList';
 import ApproveEscrow from './components/ApproveEscrow';
 import ConnectWallet from './components/ConnectWallet';
+import DocumentationPage from "./components/DocumentationPage.jsx";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <ConnectWallet />
             <Routes>
                 <Route path="/" element={<CreateEscrow />} />
+                <Route path="/docs" element={<DocumentationPage />} />
                 <Route path="/contracts" element={<ContractList />} />
                 <Route path="/contract/:contractAddress" element={<ApproveEscrow />} />
             </Routes>
